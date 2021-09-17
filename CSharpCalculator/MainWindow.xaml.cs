cffdaaff7d7ac648a9659871513a0aded8178c25
Jesse Robinson <45837967+jrobinson0529@users.noreply.github.com>
2021-09-17 11:22:07 -0500

    We have a working calculator. Needs further testing

--- CSharpCalculator/MainWindow.xaml.cs
+++ CSharpCalculator/MainWindow.xaml.cs
@@ -20,65 +20,258 @@ namespace CSharpCalculator
     /// </summary>
     public partial class MainWindow : Window
     {
-        private int _num1;
-        private int _num2;
-        private int _operation;
-        private int _solution;
+        private long _num1;
+        private long _num2;
+        private char _operatorCharacter;
+        private bool _operatorChosen = false;
+        private long _solution;
         public MainWindow()
         {
             InitializeComponent();
         }
-        private void Button_Click1(object sender, RoutedEventArgs e)
+        private void Operation()
         {
-            txtResult.Text = "1";
+            switch(_operatorCharacter)
+            {
+                case '+':
+                    _solution = _num1 + _num2;
+                    break;
+                case '-':
+                    _solution = _num1 - _num2;
+                    break;
+                case '*':
+                    _solution = _num1 * _num2;
+                    break;
+                case '/':
+                    _solution = _num1 / _num2;
+                    break;
+                default:
+                    break;
+            }
+                
         }
-        private void Button_Click2(object sender, RoutedEventArgs e)
-        {
-            txtResult.Text = "2";
-        }
-        private void Button_Click3(object sender, RoutedEventArgs e)
-        {
-            txtResult.Text = "3";
-        }
-        private void Button_Click4(object sender, RoutedEventArgs e)
-        {
-            txtResult.Text = "4";
-        }
-        private void Button_Click5(object sender, RoutedEventArgs e)
-        {
-            txtResult.Text = "5";
-        }
-        private void Button_Click6(object sender, RoutedEventArgs e)
-        {
-            txtResult.Text = "6";
-        }
-        private void Button_Click7(object sender, RoutedEventArgs e)
-        {
-            txtResult.Text = "7";
-        }
-        private void Button_Click8(object sender, RoutedEventArgs e)
-        {
-            txtResult.Text = "8";
-        }
-        private void Button_Click9(object sender, RoutedEventArgs e)
-        {
-            txtResult.Text = "9";
-        }
-        private void Button_Click0(object sender, RoutedEventArgs e)
-        {
-            txtResult.Text = "0";
-        }
-        private void Button_ClickClear(object sender, RoutedEventArgs e)
+        private void Clear()
         {
             _num1 = 0;
             _num2 = 0;
             _solution = 0;
+            _operatorCharacter = '?';
+            _operatorChosen = false;
             txtResult.Text = "0";
         }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void Clear(string textBoxDisplay)
         {
+            _num1 = 0;
+            _num2 = 0;
+            _solution = 0;
+            _operatorCharacter = '?';
+            _operatorChosen = false;
+            txtResult.Text = textBoxDisplay;
+        }
+        private void Button_Click1(object sender, RoutedEventArgs e)
+        {
+            long a;
+            if(_operatorChosen)
+            {
+                a = _num2;
+            } else
+            {
+                a = _num1;
+            }
+            
+            var b = 1;
+            var newNumber = int.Parse(a.ToString() + b.ToString());
+            txtResult.Text = newNumber.ToString();
+            if (_operatorChosen) { _num2 = newNumber; } else { _num1 = newNumber; }
+        }
+        private void Button_Click2(object sender, RoutedEventArgs e)
+        {
+            long a;
+            if (_operatorChosen)
+            {
+                a = _num2;
+            }
+            else
+            {
+                a = _num1;
+            }
 
+            var b = 2;
+            var newNumber = int.Parse(a.ToString() + b.ToString());
+            txtResult.Text = newNumber.ToString();
+            if (_operatorChosen) { _num2 = newNumber; } else { _num1 = newNumber; }
+        }
+        private void Button_Click3(object sender, RoutedEventArgs e)
+        {
+            long a;
+            if (_operatorChosen)
+            {
+                a = _num2;
+            }
+            else
+            {
+                a = _num1;
+            }
+
+            var b = 3;
+            var newNumber = int.Parse(a.ToString() + b.ToString());
+            txtResult.Text = newNumber.ToString();
+            if (_operatorChosen) { _num2 = newNumber; } else { _num1 = newNumber; }
+        }
+        private void Button_Click4(object sender, RoutedEventArgs e)
+        {
+            long a;
+            if (_operatorChosen)
+            {
+                a = _num2;
+            }
+            else
+            {
+                a = _num1;
+            }
+
+            var b = 4;
+            var newNumber = int.Parse(a.ToString() + b.ToString());
+            txtResult.Text = newNumber.ToString();
+            if (_operatorChosen) { _num2 = newNumber; } else { _num1 = newNumber; }
+        }
+        private void Button_Click5(object sender, RoutedEventArgs e)
+        {
+            long a;
+            if (_operatorChosen)
+            {
+                a = _num2;
+            }
+            else
+            {
+                a = _num1;
+            }
+
+            var b = 5;
+            var newNumber = int.Parse(a.ToString() + b.ToString());
+            txtResult.Text = newNumber.ToString();
+            if (_operatorChosen) { _num2 = newNumber; } else { _num1 = newNumber; }
+        }
+        private void Button_Click6(object sender, RoutedEventArgs e)
+        {
+            long a;
+            if (_operatorChosen)
+            {
+                a = _num2;
+            }
+            else
+            {
+                a = _num1;
+            }
+
+            var b = 6;
+            var newNumber = int.Parse(a.ToString() + b.ToString());
+            txtResult.Text = newNumber.ToString();
+            if (_operatorChosen) { _num2 = newNumber; } else { _num1 = newNumber; }
+        }
+        private void Button_Click7(object sender, RoutedEventArgs e)
+        {
+            long a;
+            if (_operatorChosen)
+            {
+                a = _num2;
+            }
+            else
+            {
+                a = _num1;
+            }
+
+            var b = 7;
+            var newNumber = int.Parse(a.ToString() + b.ToString());
+            txtResult.Text = newNumber.ToString();
+            if (_operatorChosen) { _num2 = newNumber; } else { _num1 = newNumber; }
+        }
+        private void Button_Click8(object sender, RoutedEventArgs e)
+        {
+            long a;
+            if (_operatorChosen)
+            {
+                a = _num2;
+            }
+            else
+            {
+                a = _num1;
+            }
+
+            var b = 8;
+            var newNumber = int.Parse(a.ToString() + b.ToString());
+            txtResult.Text = newNumber.ToString();
+            if (_operatorChosen) { _num2 = newNumber; } else { _num1 = newNumber; }
+        }
+        private void Button_Click9(object sender, RoutedEventArgs e)
+        {
+            long a;
+            if (_operatorChosen)
+            {
+                a = _num2;
+            }
+            else
+            {
+                a = _num1;
+            }
+
+            var b = 9;
+            var newNumber = int.Parse(a.ToString() + b.ToString());
+            txtResult.Text = newNumber.ToString();
+            if (_operatorChosen) { _num2 = newNumber; } else { _num1 = newNumber; }
+        }
+        private void Button_Click0(object sender, RoutedEventArgs e)
+        {
+            long a;
+            if (_operatorChosen)
+            {
+                a = _num2;
+            }
+            else
+            {
+                a = _num1;
+            }
+
+            var b = 0;
+            var newNumber = int.Parse(a.ToString() + b.ToString());
+            txtResult.Text = newNumber.ToString();
+            if (_operatorChosen) { _num2 = newNumber; } else { _num1 = newNumber; }
+        }
+        private void Button_ClickClear(object sender, RoutedEventArgs e)
+        {
+            Clear();
+        }
+        private void Button_ClickAddition(object sender, RoutedEventArgs e)
+        {
+            
+            _operatorCharacter = '+';
+            txtResult.Text = _operatorCharacter.ToString();
+            _operatorChosen = true;
+        }
+        private void Button_ClickSubtraction(object sender, RoutedEventArgs e)
+        {
+            _operatorCharacter = '-';
+            txtResult.Text = _operatorCharacter.ToString();
+            _operatorChosen = true;
+        }
+        private void Button_ClickMultiplication(object sender, RoutedEventArgs e)
+        {
+            _operatorCharacter = '*';
+            txtResult.Text = _operatorCharacter.ToString();
+            _operatorChosen = true;
+        }
+        private void Button_ClickDivision(object sender, RoutedEventArgs e)
+        {
+            _operatorCharacter = '/';
+            txtResult.Text = _operatorCharacter.ToString();
+            _operatorChosen = true;
+        }
+        private void Button_ClickSolution(object sender, RoutedEventArgs e)
+        {
+            Operation();
+            _num1 = _solution;
+            _num2 = 0;
+            txtResult.Text = _solution.ToString();
         }
     }
 }
